d562e45a77657ace870e5bd3228a9143de1693c8
Sergei Tsarik <mkjell@hotmail.com>
2021-10-28 22:28:04 +0300

    fix css

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -25,18 +25,18 @@ export default function Home() {
             <header><h1>Choose you character</h1></header>
         <div className="container">
             <div className="item" onClick={() => setCharacter(Cell.Character1)}>
-                <div className="cell agile stand">
+                <div className="character agile stand">
 
                 <div className="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
                 </div>
             </div>
             <div className="item" onClick={() => setCharacter(Cell.Character2)}>
-                <div className="cell wise stand">
+                <div className="character wise stand">
                 <div className="description">Lorem Ipsum is simply dummy text of </div>
                 </div>
             </div>
             <div className="item" onClick={() => setCharacter(Cell.Character3)}>
-                <div className="cell strong stand">
+                <div className="character strong stand">
                     <div className="description">Lorem Ipsum is simply dummy text of </div>
                 </div>
             </div>
