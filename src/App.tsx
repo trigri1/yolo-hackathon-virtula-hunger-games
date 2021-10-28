01f9d0657677fd35ebca35c45a55a9b7f7a2814a
Sergei Tsarik <mkjell@hotmail.com>
2021-10-28 19:56:18 +0300

    charachter selection

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
             <Game />
           </Route>
           <Route path="/">
-            <Game />
+            <Home />
           </Route>
         </Switch>
     </Router>
