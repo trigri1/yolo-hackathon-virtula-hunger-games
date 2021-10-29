4d3f503fdeed6b931ca1960e7a1b36afe28ba298
Sergei Tsarik <mkjell@hotmail.com>
2021-10-29 12:53:25 +0300

    play again button

--- src/End/index.tsx
+++ src/End/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import '../Maze/styles.css';
-import './styles.css';
+import React from "react";
+import "../Maze/styles.css";
+import useRouter from "../utils/hooks/useRouter";
+import "./styles.css";
 
 type Props = {
   wonCharacter: string;
@@ -9,14 +10,21 @@ type Props = {
 };
 
 export const EndScreen = ({ wonCharacter, won, betValue }: Props) => {
+  const router = useRouter();
+  const startGame = () => {
+    router.push("/");
+  };
   return (
     <div className="modal">
       <div className="text won">
         The winner is <span className={`cell ${wonCharacter} winner`}></span>
       </div>
       <div className="text result">
-        {won ? `YOU WON ${betValue * 3} mBTC!!!` : 'YOU LOST.. :('}
+        {won ? `YOU WON ${betValue * 3} mBTC!!!` : "YOU LOST.. :("}
       </div>
+      <button className="button" onClick={startGame}>
+        Play again
+      </button>
     </div>
   );
 };
